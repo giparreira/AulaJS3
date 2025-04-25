c331f751c2854299bdaa8205011378c657f5463d
giparreira <gi.parreirinha@gmail.com>
2025-04-24 22:50:49 -0300

    h

---
+++ app.js
@@ -0,0 +1,39 @@
+// function somaN(n1, n2) {
+//     // console.log("Executou!")
+//     // n1 = 4
+//     // n2 = 5
+//     return n1 + n2
+// }
+
+// function exibirResultado() {
+//     let resultado = somaN()
+//     console.log(resultado)
+// }
+
+// console.log(somaN(4, 5))
+// exibirResultado()
+
+// function nomeUsuario() {
+//     let nome = prompt("Digite o seu nome")
+//     return nome
+// }
+// function idadeUsuario() {
+//     let idade = prompt("Digite sua idade")
+//     return idade
+// }
+// function fraseFinal() {
+//     alert(`Olá ${nomeUsuario() || "Pessoa"}, você tem ${idadeUsuario()}`)
+// } 
+// function inicio() {
+//     fraseFinal()
+// }
+
+// inicio()
+
+let nome = "Giovana"
+
+console.log(nome.length)
+
+if (nome.length < 3) {
+
+}
